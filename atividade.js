a071c74fbb6c0eebc6c7d9b1165c5e42bec7d82c
gatinhofeiochato <152649397+gatinhofeiochato@users.noreply.github.com>
2024-04-05 18:01:19 +0000

    atividade de túlio

--- atividade.js
+++ atividade.js
@@ -1,2 +1,13 @@
-const  valor = 1
- valor = 2
+
+function soma( valor1 , valor2){
+        const resultado= valor1 + valor2
+    if(resultado>100){
+        console.log(resultado)
+    }
+}
+
+soma(120,50)
+
+
+
+
